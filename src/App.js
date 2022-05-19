3958a5bf767596aebc56f5b3155a87939e26aacc
Olena Grynevych <ezayak@gmail.com>
2022-05-19 22:47:10 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Router } from 'react-router-dom';
 import './App.css';
 import { CategoryPage } from './routes/category-page/category-page.component';
 import { Navigation } from './routes/navigation/navigation.component';
 
 function App() {
     return (
-        <Router basename='/trainee-task'>
-            <Routes>
-                <Route path='/' element={ <Navigation /> }>
-                    <Route index element={ <CategoryPage /> } />
-                </Route>
-            </Routes>            
-        </Router>
+        <Routes>
+            <Route path='/' element={ <Navigation /> }>
+                <Route index element={ <CategoryPage /> } />
+            </Route>
+        </Routes>            
     );
 }
 
